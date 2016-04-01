8d8c133d11f319236f00003eb61fba78ba276c4b
Di Tian <peacefultd@gmail.com>
2016-03-31 22:58:49 -0700

    +

--- chapter1.scm
+++ chapter1.scm
@@ -137,5 +137,31 @@
 ;When x = 0, improved_guess = 2/3 * guess, that is, guess(i+1) = 2/3 * guess(i). Therefore |improved_guess - guess| = 1/3 * guess, 
 ;and is always greater than 0.000001 * guess, resulting in infinite interation here. x = 0 should be specifically taken care of.
 
+;-----
+;1.9
+;-----
+(+ 4 5)
+= (+ 3 5) + 1
+= (+ 2 5) + 1 + 1
+= (+ 1 5) + 1 + 1 + 1
+= (+ 0 5) + 1 + 1 + 1 + 1
+= 5 + 1 + 1 + 1 + 1
+= 6 + 1 + 1 + 1
+= 7 + 1 + 1
+= 8 + 1
+= 9
+; There are deferred evaluations, which are "inc (+ 3 5)", "inc (+ 2 5)", "inc (+ 1 5)" and "inc (+ 0 5)", therefore this process is recursive.
+; Expansion then contraction; a chain of deferred operations.
 
+(+ 4 5)
+= (+ 3 6)
+= (+ 2 7)
+= (+ 1 8)
+= (+ 0 9)
+= 9
+; No expansion then contraction. All we need to keep track of are the current values of the variables a and b.
+; The state of the process can be summarized by a fixed number of state variables, together with the updating rule.
 
+;-----
+;1.10
+;-----
