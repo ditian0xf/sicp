ec3b860743a6ca127fddd014edf2dd6732ac23e8
Di Tian <peacefultd@gmail.com>
2016-04-02 03:15:34 -0700

    +

--- chapter1.scm
+++ chapter1.scm
@@ -244,3 +244,14 @@
 (define (pascal-element row col) 
   (if (or (= col 0) (= row col)) 1 
       (+ (pascal-element (- row 1) (- col 1)) (pascal-element (- row 1) col))))
+
+;-----
+;1.13
+;-----
+; Step 1. Prove by induction.
+; Step 2. To prove Fib(n) = (a^n - b^n) / sqrt(5) is the closest integer to a^n / sqrt(5),
+;         that is, to prove |(a^n - b^n) / sqrt(5) - a^n / sqrt(5)| <= 0.5.
+
+;-----
+;1.14
+;----- 
