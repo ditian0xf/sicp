4fb7fda523b880aa187be5b772329acea6feed60
Di Tian <peacefultd@gmail.com>
2016-04-09 15:16:01 -0700

    +

--- chapter1.scm
+++ chapter1.scm
@@ -592,3 +592,9 @@ R3  ; curCount = 18.
 ;-----
 ;1.25
 ;-----
+; The result is correct, but the execution time is larger.
+; For the "fast-expt" method, we are doing (base^exp) mod m, however, evaluating (base^exp) is slow and not necessary.
+; For the "expmod" method, we are doing (something) mod m. It can be easily observed that (something) never exceeds base^2.
+; Althogh (base^exp) mod m is evaluated only once, while (something) mod m is evaluated log(exp) times, 
+; the latter case is still much faster, because dealing with huge number arithmatic operations is so slow.
+
