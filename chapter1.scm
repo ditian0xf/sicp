f0538a4b7a9ce6642ddd1a00b7446b3967cdfe3b
Di Tian <peacefultd@gmail.com>
2016-05-07 13:21:47 -0700

    +

--- chapter1.scm
+++ chapter1.scm
@@ -680,3 +680,14 @@ R3  ; curCount = 18.
 ;-----
 ;1.30
 ;-----
+(define (sum term a next b)
+  (define (iter a result) 
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+;-----
+;1.31
+;-----
+
