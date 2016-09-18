057e25d50fa81667b15178ed1a1d2a758e92b23a
Di Tian <peacefultd@gmail.com>
2016-09-17 20:30:51 -0700

    2.2 -- 2.3

--- chapter2/2_point_and_segment.scm
+++ chapter2/2_point_and_segment.scm
@@ -12,6 +12,10 @@
 (define (y-point p)
   (cdr p))
 
+(define (equal-point p1 p2)
+  (and (= (x-point p1) (x-point p2))
+       (= (y-point p1) (y-point p2))))
+
 (define (print-point p)
   (newline)
   (display "(")
@@ -38,6 +42,25 @@
           (x-end (x-point end))
           (y-end (y-point end)))
       (make-point (average x-start x-end) (average y-start y-end)))))
+
+(define (perpendicular?　seg1 seg2)
+  (let ((start1 (start-segment seg1))
+        (end1 (end-segment seg1))
+        (start2 (start-segment seg2))
+        (end2 (end-segment seg2)))
+    (let ((vector1 (cons (- (x-point end1) (x-point start1))
+                         (- (y-point end1) (y-point start1))))
+          (vector2 (cons (- (x-point end2) (x-point start2))
+                         (- (y-point end2) (y-point start2)))))
+      (= 0 (+ (* (car vector1) (car vector2))
+              (* (cdr vector1) (cdr vector2)))))))
+
+(define (segment-length seg)
+  (let ((start (start-segment seg))
+        (end (end-segment seg)))
+    (sqrt (+ (expt (- (x-point start) (x-point end)) 2)
+             (expt (- (y-point start) (y-point end)) 2)))))
+
 
 ; test
 ;(define p1 (make-point 0.5 -1.8))
--- chapter2/3.scm
+++ chapter2/3.scm
@@ -4,4 +4,80 @@
 
 (load "2_point_and_segment.scm")
 
-(make-point 2 2)
+; perimeter of rectangle
+(define (perimeter rectangle)
+  (let ((dim1 (get-dim1 rectangle)) (dim2 (get-dim2 rectangle)))
+    (* 2 (+ dim1 dim2))))
+
+; area of rectangle
+(define (area rectangle)
+  (let ((dim1 (get-dim1 rectangle)) (dim2 (get-dim2 rectangle)))
+    (* dim1 dim2)))
+
+; representation 1
+; --- seg1 ---
+; |      
+; |
+; seg2
+; |
+(define (alert)
+  ;(newline)
+  (display "not a valid rectangle!"))
+
+(define (make-rectangle seg1 seg2)
+  (if (or (not (equal-point (start-segment seg1) (start-segment seg2)))  ; not started at the same point
+          (not (perpendicular? seg1 seg2)))  ; or not perpendicular to each other
+      (alert)
+      (cons seg1 seg2)))
+
+(define (get-dim1 rectangle)
+  (segment-length (car rectangle)))
+
+(define (get-dim2 rectangle)
+  (segment-length (cdr rectangle)))
+
+#| test
+> (define seg1 (make-segment (make-point 0 0) (make-point 0 5)))
+> (define seg2 (make-segment (make-point 0 0) (make-point 8 0)))
+> (define rectangle (make-rectangle seg1 seg2))
+> rectangle
+(((0 . 0) 0 . 5) (0 . 0) 8 . 0)
+> (perimeter rectangle)
+26
+> (area rectangle)
+40
+> (define seg2 (make-segment (make-point 0 1) (make-point 8 0)))
+> (define rectangle (make-rectangle seg1 seg2))
+not a valid rectangle!
+> (define seg2 (make-segment (make-point 0 0) (make-point 8 -1)))
+> (define rectangle (make-rectangle seg1 seg2))
+not a valid rectangle!
+>
+|#
+
+; representation 2
+; origin --- vertex1
+;   |
+;   |
+;   |
+; vertex2
+(define (make-rectangle origin vertex1 vertex2)
+  (if (not (perpendicular? (make-segment origin vertex1) (make-segment origin vertex2)))
+      (alert)
+      (cons origin (cons vertex1 vertex2))))
+
+(define (get-dim1 rectangle)
+  (segment-length (make-segment (car rectangle) (car (cdr rectangle)))))
+
+(define (get-dim2 rectangle)
+  (segment-length (make-segment (car rectangle) (cdr (cdr rectangle)))))
+
+#| test
+> (define rectangle (make-rectangle (make-point 0 0) (make-point 0 5) (make-point 8 0)))
+> (perimeter rectangle)
+26
+> (area rectangle)
+40
+> (define rectangle (make-rectangle (make-point 0 0) (make-point 1 5) (make-point 8 0)))
+not a valid rectangle!
+|#
