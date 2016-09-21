192e3fdf1db116857eb829ddf2e68ab6f3a14955
Di Tian <peacefultd@gmail.com>
2016-09-21 02:19:40 -0700

    div by interval spans zero: 1/upper > 1/lower, not right

--- chapter2/10-spans-zero.scm
+++ chapter2/10-spans-zero.scm
@@ -13,12 +13,12 @@
                    (max p1 p2 p3 p4))))
 
 (define (div-interval x y)
-  (if (= 0 (- (upper-bound y) (lower-bound y)))
+  (if (and (<= (lower-bound y) 0) (>= (upper-bound y) 0))
       (display "undefined: divided by interval that spans zero")
       (mul-interval
        x
-       (make-interval (/ 1.0 (upper-bound y))
-                      (/ 1.0 (lower-bound y))))))
+       (make-interval (/ 1.0 (upper-bound y))      ; spans 0: 1/upper-bound > 1/lower-bound
+                      (/ 1.0 (lower-bound y))))))  ; meaningless
 
 #| test
 
@@ -26,7 +26,7 @@
 > (define interval2 (make-interval 16 32))
 > (div-interval interval1 interval2)
 (0.0625 . 0.25)
-> (define interval2 (make-interval 16 16))
+> (define interval2 (make-interval -16 16))
 > (div-interval interval1 interval2)
 undefined: divided by interval that spans zero
 
