bd3d4ada6a81fcd7d1d27c3e4a4980acc69409a3
Di Tian <peacefultd@gmail.com>
2016-09-24 23:25:06 -0700

    2.15

--- chapter2/15.scm
+++ chapter2/15.scm
@@ -2,5 +2,21 @@
 ;2.15
 ;-----
 
-; she is right.
-; 
+; she is right
+
+; two intervals with non-zero error:
+
+;   addition, subtraction:
+;   the new width is (width1 + width2)
+
+;   multiplication, division(if two percentage tolerances are small):
+;   the new percentage tolerance is (tolerance1 + tolerance2)
+
+
+; if one interval is with zero error:
+
+;   error of the result remains unchanged
+
+
+; arithmetic operations between two intervals with non-zero error introduces bigger error
+; should minimize these kind of operations
