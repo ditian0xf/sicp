239b9b8270b22cefe0bedeec2e490b50d8b84ce0
Di Tian <peacefultd@gmail.com>
2016-10-02 15:35:14 -0700

    2.27

--- chapter2/27.scm
+++ chapter2/27.scm
@@ -2,7 +2,26 @@
 ;2.27
 ;-----
 
+; example walk throughs in 27.png
+
 (define (deep-reverse x)
   (cond ((null? x) x)  ; input is an empty list
         ((not (pair? x)) x)  ; input is a single number (NOT a one-item list); this happens near the end of recursion
-        (else (list (deep-reverse (cdr x)) (deep-reverse (car x))))))
+        (else (append (deep-reverse (cdr x)) (list (deep-reverse (car x)))))))
+
+; tests
+; > (deep-reverse (list))
+; ()
+; > (deep-reverse (list 3))
+;(3)
+; > (deep-reverse (list (list 3) (list)))
+; (() (3))
+; > (deep-reverse (list (list 1 2) (list)))
+; (() (2 1))
+; > (deep-reverse (list (list 3 4) (list 5)))
+; ((5) (4 3))
+; > (deep-reverse (list (list 1 2) (list 3 4)))
+; ((4 3) (2 1))
+; > (deep-reverse (list (list 1 2) (list 3 (list 4 5)) 6))
+; (6 ((5 4) 3) (2 1))
+; >
