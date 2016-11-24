d0dd292ddb6bc9683d23ffc57d35373236fd45cc
ditian <peacefultd@gmail.com>
2016-11-23 23:48:05 -0800

    2.63 some more progress

--- chapter2/2.63.scm
+++ chapter2/2.63.scm
@@ -45,3 +45,29 @@
 ; > (adjoin-set 4 s)
 ; (2 (1 () ()) (3 () (4 () ())))
 ; >
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree) result-list)))))
+  (copy-to-list tree '()))
+
+; ----- a -----
+; Yes the two procedures produce the same result for every tree.
+; For trees in Figure 2.16,
+; tree->list-1 produces [1, 3, 5, 7, 9, 11],
+; tree->list-2 produces [1, 3, 5, 7, 9, 11].
+
+; ----- b -----
+
+
