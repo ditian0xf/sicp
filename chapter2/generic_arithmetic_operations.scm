efdaaf393419aa471b01cbaa1cf34b161740d019
ditian <peacefultd@gmail.com>
2018-02-28 00:02:03 -0800

    save

--- chapter2/generic_arithmetic_operations.scm
+++ chapter2/generic_arithmetic_operations.scm
@@ -4,6 +4,28 @@
 
 (load "attach_tag.scm")
 
+; ----- Naive implementation of put/get (Hashtable) ------
+(define global-array '())
+
+(define (make-entry k v) (list k v))
+(define (key entry) (car entry))
+(define (value entry) (cadr entry))
+
+(define (put op type item)
+  (define (put-helper k array)
+    (cond ((null? array) (list(make-entry k item)))
+          ((equal? (key (car array)) k) array)
+          (else (cons (car array) (put-helper k (cdr array))))))
+  (set! global-array (put-helper (list op type) global-array)))
+
+(define (get op type)
+  (define (get-helper k array)
+    (cond ((null? array) #f)
+          ((equal? (key (car array)) k) (value (car array)))
+          (else (get-helper k (cdr array)))))
+  (get-helper (list op type) global-array))
+; --------------------------------------------------------
+
 (define (add x y) (apply-generic 'add x y))
 (define (sub x y) (apply-generic 'sub x y))
 (define (mul x y) (apply-generic 'mul x y))
@@ -21,3 +43,6 @@
        (lambda (x y) (tag (/ x y))))
   (put 'make 'scheme-number (lambda (x) (tag x)))
   'done)
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
+
